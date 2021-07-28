6373174781079e2d8eeeb46b3eda371d565821ff
PrinceOfGit <ShandorUk30@yahoo.com>
2021-07-28 20:47:00 +0300

    My first commit in this homework

---
+++ mainFile.js
@@ -0,0 +1,108 @@
+// Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+//
+//     - Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// -- отримує текст з параграфа з id "content"
+// -- отримує текст з блоку з id "rules"
+// -- замініть текст параграфа з id 'content' на будь-який інший
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+// -- змініть кожному елементу колір фону на червоний
+// -- змініть кожному елементу колір тексту на синій
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// -- отримати всі елементи з класом fc_rules. визначити їм 2 події "клік", одна вивдоить текст елементу, інша довільний ьексь
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+
+
+//let contentText = document.getElementById('content');
+
+//console.log(contentText);
+
+//let  rulesText = document.getElementById('rules');
+
+//console.log(rulesText);
+
+//contentText.innerText = 'You shall not pass';
+
+//rulesText.innerText = 'The ring that rules them all';
+
+
+// contentText.style.backgroundColor = '#FF0000';
+//
+// rulesText.style.backgroundColor = '#FF0000';
+//
+//
+//
+// let changeColor = document.getElementsByClassName('fc_rules');
+//
+// function changeColorFun () {
+//     for (let i = 0; i < changeColor.length; i++) {
+//         changeColor[i].style.backgroundColor = '#FF0000'
+//     }
+// }
+//
+// changeColorFun()
+//
+// console.log(changeColor);
+
+// contentText.style.color = '#7B68EE';
+// rulesText.style.color = '#7B68EE';
+//
+// let changeTextColor = document.getElementsByClassName('fc_rules');
+// function TextColor () {
+//     for (let i = 0; i < changeTextColor.length; i++) {
+//         changeTextColor[i].style.color = '#7B68EE'
+//     }
+// }
+//
+// TextColor ()
+
+
+// let getClass = document.getElementsByClassName('fc_rules');
+//
+// for (const getLi of getClass) {
+//      getLi.addEventListener( 'click', () => {
+//          console.log(getLi.innerText);
+//      } );
+//      getLi.addEventListener( 'click', () => {
+//          console.log('Hello pals!');
+//      } );
+//
+//  }
+
+
+ // let changeTextColor2 = document.getElementsByClassName('fc_rules');
+ // function TextColor2 () {
+ //     for (let i = 0; i < changeTextColor2.length; i++) {
+ //         changeTextColor2[i].style.color = '#FF0000'
+ //     }
+ // }
+ //
+ // TextColor2()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
